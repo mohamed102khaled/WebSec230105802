e56d27e5a971a3fa71c4df0faa9ca5badf989f40
mohamed <mohamed102khaled@gmail.com>
2025-02-22 23:03:25 +0200

    last update

--- WebSecService/routes/web.php
+++ WebSecService/routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/text', function () {
+    return view('text'); //multable.blade.php
+});
+
+Route::get('/multable', function () {
+    return view('multable'); //multable.blade.php
+});
+
+Route::get('/even', function () {
+    return view('even'); //even.blade.php
+});
+    
+Route::get('/prime', function () {
+    return view('prime'); //prime.blade.php
+});
+    
