e6c49d809f4a80a740e6d65e801d8864f6c2fd85
mohamed102khaled <mohamed102khaled@gmail.com>
2025-04-25 19:42:02 +0300

    code

--- WebSecService/app/Models/User.php
+++ WebSecService/app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable
     public function role()
     {
         return $this->belongsTo(Role::class, 'role_id');
+        
     }
 
     protected $fillable = ['name', 'email', 'role', 'password'];
